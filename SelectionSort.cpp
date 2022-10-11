2400e8204ef171f6238866453ffc06876eee6299
Aman Patel <106889345+Amanpatel2529@users.noreply.github.com>
2022-10-11 16:24:03 +0530

    Create SelectionSort.cpp

---
+++ SelectionSort.cpp
@@ -0,0 +1,35 @@
+/*let's do it Aman*/
+
+//Simple selection sort demo for 10 random doubles and int
+
+#include <generic.h>
+#include <selectionSort.h>
+
+int main()
+{
+    const int MAX_ELEMENTS = 10;
+    int arr[MAX_ELEMENTS];
+    double arrD[MAX_ELEMENTS];
+
+    //Filling up the array with random numbers;
+    for (int i = 0; i < MAX_ELEMENTS; ++i)
+    {   
+        arr[i] = algo::random_range(1, 100);
+        arrD[i] = algo::random_range(1.0, 99.99);
+    }
+
+    std::cout << "Before Sorting:\n";
+    algo::printList(arr, MAX_ELEMENTS);
+    algo::selectionSort(arr, 0, MAX_ELEMENTS - 1);
+    std::cout << "After Sorting:\n";
+    algo::printList(arr, MAX_ELEMENTS);
+
+
+    std::cout << "\n\nBefore Sorting:\n";
+    algo::printList(arrD, MAX_ELEMENTS);
+    algo::selectionSort(arrD, 0, MAX_ELEMENTS - 1);
+    std::cout << "After Sorting:\n";
+    algo::printList(arrD, MAX_ELEMENTS);
+    return 0;
+}
+
